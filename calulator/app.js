62b7cf902f7252c0f45b022cd2175d5ebf231448
David Spencer <davidspencer@Davids-MacBook-Pro.local>
2015-04-03 10:21:17 -0700

    updated calulator and news_feed folders

--- calulator/app.js
+++ calulator/app.js
@@ -1,32 +1,53 @@
-// Require the modules we're going to need:
+// required variables
 var express = require("express"),
-    ejs = require("ejs"),
-    bodyParser = require("body-parser"),
-    methodOverride = require("method-override");
-
-// Now instantiate our express app:
+	ejs = require("ejs"),
+	bodyParser = require("body-parser"),
+	methodOverride = require("method-override");
+// Now instantiate the exoress app
 var app = express();
-
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false}));
 
-var classmates = []	
-//Set root route
-app.get('/', function(req, res) {
 
-    res.render("index", { classmates: classmates});
+app.get('/', function(req, res){
+	res.render("index");
 });
 
 app.post('/add', function(req, res){
-	//req.params //request params
-	//req.query // query params
-	//req.body // body params
-	classmates.push(req.body);
+	var x = Number(req.body.add1);
+	var y = Number(req.body.add2);
+	var sum = x + y;
+	res.send("The Sum is: " + sum);
 	res.redirect('/');
 });
 
+app.post('/subtract', function(req, res){
+	var x = Number(req.body.sub1);
+	var y = Number(req.body.sub2);
+	var sub = x - y;
+	res.send("The difference is: " + sub);
+	res.redirect('/');
+});
+
+app.post('/multiply', function(req, res){
+	var x = Number(req.body.mult1);
+	var y = Number(req.body.mult2);
+	var mult = x * y;
+	res.send("The multiple is: " + mult)
+	res.redirect('/');
+});
+
+app.post('/divide', function(req, res){
+	var x = Number(req.body.div1);
+	var y = Number(req.body.div2);
+	var div = x / y;
+	res.send("The result is: " + div);
+	res.redirect('/');
+});
+
+
 app.listen(3000, function(){
-	console.log('Server listening on port 3000');
+	console.log('=====LISTENING=====');
 });
--- warmups.js
+++ warmups.js
@@ -7,3 +7,85 @@ function simpleSearch(arr , val){
         
     }
 }
+
+
+
+var binSearch = function (arr,val) {
+
+  var min = 0;
+  var max = arr.length;
+  var mid = Math.floor((max+min)/2);
+
+  while(min <= max) {
+    if (val === arr[mid]) {
+      return mid;
+    } else if (val < arr[mid]) {
+      max = mid - 1;
+      mid = Math.floor((max+min)/2);
+    } else {
+      min = mid + 1;
+      mid = Math.floor((max+min)/2);
+    }
+  }
+
+  return -1;
+};
+
+
+
+var merge = function(left, right){
+    var result  = [],
+        il      = 0,
+        ir      = 0;
+
+    // So first we need to loop through both arrays, adding the element that is smallest
+    while (il < left.length && ir < right.length){
+        if (left[il] < right[ir]){ // If left is less than right, add the left element
+            result.push(left[il]);
+            il += 1;
+        } else { // Else add the right element
+            result.push(right[ir]);
+            ir +=1;
+        }
+    }
+
+    // Now we add on any elements that remain at the end of one array or the other.
+    if (il < left.length) {
+        for (var i = il; i < left.length; i++) {
+            result.push(left[i]);
+        }
+        return result;
+    } else if(ir < right.length) {
+        for (var i = ir; i < right.length; i++) {
+            result.push(right[i]);
+        }   
+        return result;
+    } else {
+        return result;
+    }
+}
+
+
+// And we can test this with:
+
+merge([3, 8, 22], [1, 5, 7]); // returns [1, 3, 5, 7, 8, 22];
+
+merge([6, 7, 50, 55], [2, 12, 35, 71]); // returns [2, 6, 7, 12, 35, 50, 55, 71]
+
+
+// A fun version using .concat
+var merge2 = function(left, right){
+    var result  = [],
+        il      = 0,
+        ir      = 0;
+
+    while (il < left.length && ir < right.length){
+        if (left[il] < right[ir]){
+            result.push(left[il++]);
+        } else {
+            result.push(right[ir++]);
+        }
+    }
+
+    return result.concat(left.slice(il)).concat(right.slice(ir));
+}
