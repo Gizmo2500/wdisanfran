b40a6678d9e91ac4dde2a2b8c3faf09fe936848a
David Spencer <davidspencer@Davids-MacBook-Pro.local>
2015-04-11 16:25:38 -0700

    added routes to project 1

--- project1/app.js
+++ project1/app.js
@@ -2,7 +2,8 @@
 		bodyParser = require('body-parser'),
 		session = require('express-session'),
 		request = require('request'),
-		pg = require('pg');
+		pg = require('pg'),
+		db = require('./models');
 
 	var app = express();
 	//view engine for the app
@@ -18,11 +19,26 @@
 	app.get('/', function(req, res){
 		res.render("index", {title: "My title"});
 	});
-
+	// route to render search page
 	app.get('/search', function(req,res){
 		res.render('search');
 	});
 
+
+	//login route
+	app.get('/login', function(req, res){
+		res.send("login Page");
+	});
+
+	//sign up route
+	app.get('/signup', function(req, res){
+		res.send('sign up page');
+	});
+
+	//profile route
+	app.get('/profile', function(req,res){
+		res.send('profile page');
+	});
 
 	app.listen(3000, function(){
 		console.log('Server is running');
